af66aa5bc45d4fa1b725dabc2d49a09413aedf4c
HasutoSasaki <hasuto.sa@gmail.com>
2025-05-01 11:35:44 +0900

    fix: Remove unused import img.

--- assets/assets.ts
+++ assets/assets.ts
@@ -1,6 +1,5 @@
 import myIcon from './image/my-icon.png'
 import contactArrow from './image/contact-arrow.svg'
-import navBg from './image/nav-bg.png'
 import moonIcon from './image/moon-icon.svg'
 import menuIcon from './image/menu-icon.svg'
 import closeIcon from './image/close-icon.svg'
@@ -9,7 +8,6 @@ import profile from './image/profile.png'
 export const assets = {
     myIcon,
     contactArrow,
-    navBg,
     moonIcon,
     menuIcon,
     closeIcon,
