00327f74df033120cada0d40915673c27cf0b9b3
HasutoSasaki <hasuto.sa@gmail.com>
2025-05-30 08:23:22 +0900

    feat: Implement dark mode functionality and update layout components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React, { use, useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
@@ -8,15 +9,37 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.them === 'dark' || (!('them' in localStorage)
+      && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.them = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.them = '';
+    }
+  }, [isDarkMode]);
+
   return (
     <>
-      <Navbar />
-      <Header />
-      <About />
-      <Services />
-      <Work />
-      <Contact />
-      <Footer />
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Header isDarkMode={isDarkMode} />
+      <About isDarkMode={isDarkMode} />
+      <Services isDarkMode={isDarkMode} />
+      <Work isDarkMode={isDarkMode} />
+      <Contact isDarkMode={isDarkMode} />
+      <Footer isDarkMode={isDarkMode} />
     </>
   );
 }
