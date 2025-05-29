933671307422867b8c690c4561f0724b79b0ff71
HasutoSasaki <hasuto.sa@gmail.com>
2025-05-30 07:36:04 +0900

    feat: Update Footer component with social media links and copyright notice.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -13,6 +13,19 @@ const Footer = () => {
                     hasuto.sa@gmail.com
                 </div>
             </div>
+
+            <div className='text-center sm:flex items-center justify-between border-t
+            border-gray-400 mx-[10%] mt-12 py-6'>
+                <p>© 2025 Hasuto Sasaki. All rights reserved.</p>
+                <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                    <li><a target='_blank' href="https://instagra.com/">
+                        GitHub</a></li>
+                    <li><a target='_blank' href="https://instagra.com/">
+                        LinkedIn</a></li>
+                    <li><a target='_blank' href="https://instagra.com/">
+                        Twitter</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
