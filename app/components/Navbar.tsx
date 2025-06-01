f2f8a3f08c118bd79bd074d4f23d46bd0d4be365
HasutoSasaki <hasuto.sa@gmail.com>
2025-06-01 09:28:40 +0900

    feat: Update mobile menu close icon for dark mode support

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -85,10 +85,11 @@ const Navbar = ({ isDarkMode, setIsDarkMode }: NavbarProps) => {
 
                 {/* -- ---- mobile menu ---- -- */}
                 <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64
-                top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+                top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500
+                dark:bg-darkHover dark:text-white'>
 
                     <div className='absolute top-6 right-6' onClick={closeMenu}>
-                        <Image src={assets.closeIcon} alt='Close Menu'
+                        <Image src={isDarkMode ? assets.closeWhite : assets.closeBlack} alt='Close Menu'
                             className='w-5 cursor-pointer' />
                     </div>
 
