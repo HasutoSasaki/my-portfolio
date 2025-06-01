b0d6d06d7d244c2a6ef7268aa4dfc1d59a3fc613
HasutoSasaki <hasuto.sa@gmail.com>
2025-06-01 09:29:15 +0900

    feat: Add dark mode support to Work component

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = () => {
+const Work = ({ isDarkMode }: { isDarkMode: boolean }) => {
     return (
         <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>
@@ -13,7 +13,7 @@ const Work = () => {
                 I am a frontend Developer from Tokyo, Japan with 2 years of experience in the field.
             </p>
 
-            <div className='grid grid-cols-auto my-10 gap-5'>
+            <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
                 {workData.map((project, index) => (
                     <div key={index}
                         className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg
@@ -37,9 +37,10 @@ const Work = () => {
             </div>
 
             <a href="" className="w-max flex items-center justify-center gap-2
-            text-gray-700 border-[0.5px] border-gray-700 rounded-full y-3 px-10 mx-auto
-            my-20 hover:bg-lightHover duration-500">
-                Show more <Image src={assets.rightArrowBold} alt='right arrow' className='w-4' />
+            text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto
+            my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover">
+                Show more
+                <Image src={isDarkMode ? assets.rightArrowBoldDark : assets.rightArrowBold} alt='right arrow' className='w-4' />
             </a>
         </div>
     )
