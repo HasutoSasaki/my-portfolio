4d8b38a7237daf928a45914dcbfc6c1d6d9c9070
HasutoSasaki <hasuto.sa@gmail.com>
2025-06-01 09:28:51 +0900

    feat: Add dark mode support for contact and resume buttons in Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,12 +20,12 @@ const Header = () => {
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
                 <a href="#contact"
                     className='px-10 py-3 border border-white rounded-full bg-black
-                    text-white flex items-center gap-2'>contact me
+                    text-white flex items-center gap-2 dark:bg-transparent'>contact me
                     <Image src={assets.contactArrow} alt='Contact Arrow' className='w-4' />
                 </a>
                 <a href="/sample.pdf" download
                     className='px-10 py-3 border rounded-full border-gray-500 flex
-                        items-center gap-2'>my resume
+                        items-center gap-2 bg-white dark:text-black'>my resume
                     <Image src={assets.contactArrow} alt='Contact Arrow' className='w-4' />
                 </a>
             </div>
