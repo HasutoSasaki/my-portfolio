a44fa3860a2c48696870b61e8608801caed7c9e0
HasutoSasaki <hasuto.sa@gmail.com>
2025-06-03 06:56:32 +0900

    feat: Refactor Header component for improved GSAP animations and update profile description.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef } from 'react'
+import { useRef, createRef } from 'react'
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
 import gsap from 'gsap'
@@ -9,40 +9,48 @@ import { useGSAP } from '@gsap/react'
 const Header = () => {
     const messageRef = useRef(null)
     const imageContainerRef = useRef(null)
+    const profileImageRef = useRef(null)
+    const imageBorderRef = useRef(null)
     const greeting = "Hi! I'm Hasuto Sasaki"
+    const charRefs = useRef(greeting.split('').map(() => createRef<HTMLSpanElement>()));
 
     useGSAP(() => {
-        // プロフィール画像の円形描画アニメーション
         const imageTimeline = gsap.timeline();
 
-        // 最初の状態設定: 画像は非表示、ボーダーも0%
-        gsap.set('.profile-img', { opacity: 0 });
-        gsap.set('.image-border', { strokeDasharray: 1000, strokeDashoffset: 1000, opacity: 1 });
+        const circumference = 1000;
 
-        // 1. ボーダーを描画するアニメーション
-        imageTimeline.to('.image-border', {
+        gsap.set(profileImageRef.current, { opacity: 0 });
+
+        gsap.set(imageBorderRef.current, {
+            strokeDasharray: circumference,
+            strokeDashoffset: circumference,
+            opacity: 1
+        });
+
+        // 1. 円を描画
+        imageTimeline.to(imageBorderRef.current, {
             strokeDashoffset: 0,
             duration: 1.5,
-            ease: 'power2.inOut'
+            ease: 'power1.inOut'
         });
 
-        // 2. 画像をフェードイン
-        imageTimeline.to('.profile-img', {
-            opacity: 1,
-            duration: 1,
-            ease: 'power2.inOut'
-        });
-
-        // 3. border fade out
-        imageTimeline.to('.image-border', {
+        // 2. ボーダー描画が完了したら、ボーダーを非表示にする
+        imageTimeline.to(imageBorderRef.current, {
             opacity: 0,
             duration: 0.5,
             ease: 'power2.inOut'
-        });
+        }, "fadeInStart");
+
+        // 3. 画像をフェードイン
+        imageTimeline.to(profileImageRef.current, {
+            opacity: 1,
+            duration: 1,
+            ease: 'power2.inOut'
+        }, "fadeInStart");
 
 
         // 各文字に対するアニメーション
-        const chars = gsap.utils.toArray('.char');
+        const chars = charRefs.current.map(ref => ref.current);
         gsap.set(chars, { opacity: 0, scale: 0, x: -50 });
         gsap.to(chars, {
             opacity: 1,
@@ -51,42 +59,43 @@ const Header = () => {
             stagger: 0.05,
             duration: 0.5,
             ease: 'back.out(1.7)',
-            delay: 2.5
-        })
+            delay: 1.3
+        });
     });
     return (
         <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col
         items-center justify-center gap-4'>
-            <div ref={imageContainerRef} className="relative w-32 h-32 flex items-center justify-center">
-                <svg className="absolute w-full h-full" viewBox="0 0 100 100">
-                    <circle
-                        className="image-border"
+            <div ref={imageContainerRef} className="relative w-32 h-40 flex items-center justify-center">
+                <svg className="absolute w-full h-full" viewBox="0 0 100 125">
+                    <ellipse
+                        ref={imageBorderRef}
                         cx="50"
-                        cy="50"
-                        r="48"
+                        cy="62"
+                        rx="49"
+                        ry="61"
                         fill="none"
                         stroke="white"
-                        strokeWidth="2"
+                        strokeWidth="1"
                     />
                 </svg>
                 <Image
+                    ref={profileImageRef}
                     src={assets.profile}
                     alt='Profile Image'
-                    className='rounded-full w-full h-full object-cover profile-img'
+                    className='rounded-full w-full h-full object-cover'
                 />
             </div>
             <h3 ref={messageRef} className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
                 {greeting.split('').map((char, index) => (
-                    <span key={index} className="char inline-block font-Ovo">
+                    <span key={index} ref={charRefs.current[index]} className="inline-block font-Ovo">
                         {char === ' ' ? '\u00A0' : char}
                     </span>
                 ))}
             </h3>
             <h1 className='text-3xl sm:text-6xl lg-text-[66px] font-Ovo'>
-                frontend web developer based in Japan.</h1>
+                software developer based in Japan.</h1>
             <p className='max-w-2xl mx-auto font-Ovo'>
-                I am a frontend web developer from Japan, Japan with 10 years of experience
-                in multiple companies like Microsoft,
+                I am a software developer from Japan with 2 years experience in an SES company.
             </p>
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
                 <a href="#contact"
@@ -94,7 +103,7 @@ const Header = () => {
                     text-white flex items-center gap-2 dark:bg-transparent'>contact me
                     <Image src={assets.contactArrow} alt='Contact Arrow' className='w-4' />
                 </a>
-                <a href="/sample.pdf" download
+                <a href="/resume.pdf" download
                     className='px-10 py-3 border rounded-full border-gray-500 flex
                         items-center gap-2 bg-white dark:text-black'>my resume
                     <Image src={assets.contactArrow} alt='Contact Arrow' className='w-4' />
