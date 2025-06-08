37c2c5e9c01beac465018ca581bc19f3f5a5834e
HasutoSasaki <hasuto.sa@gmail.com>
2025-06-08 15:14:38 +0900

    fix: Comment out unused contact link in ActionButtons component.

--- app/components/header/ActionButtons.tsx
+++ app/components/header/ActionButtons.tsx
@@ -6,11 +6,11 @@ import { assets } from '@/assets/assets'
 export const ActionButtons = () => {
     return (
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a href="#contact"
+            {/* <a href="#contact"
                 className='px-10 py-3 border border-white rounded-full bg-black
                 text-white flex items-center gap-2 dark:bg-transparent'>contact me
                 <Image src={assets.contactArrow} alt='Contact Arrow' className='w-4' />
-            </a>
+            </a> */}
             <a href="/resume.pdf" download
                 className='px-10 py-3 border rounded-full border-gray-500 flex
                     items-center gap-2 bg-white dark:text-black'>my resume
